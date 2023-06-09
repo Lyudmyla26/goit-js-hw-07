f2826c5888a4a9be6c4c5a4716c6f62c3d342bf0
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-06-09 16:58:30 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const el = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items.map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item "> 
+<a href="${original}" class="gallery__link">
+<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+//     </a></li>`
+  );
+}
+const markup = createGalleryMarkup(galleryItems);
+el.innerHTML = markup.join("");
+el.addEventListener("click", onClick);
+function onClick(event) {
+  event.preventDefault();
+
+  const instance =
+    basicLightbox.create(`<img  src="${event.target.dataset.source}"  width='800'>
+  `);
+
+  instance.show();
+  if (instance.visible()) {
+    window.addEventListener("keydown", onEscapeClose);
+  }
+
+  function onEscapeClose(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
